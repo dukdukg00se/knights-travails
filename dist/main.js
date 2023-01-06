43baba9147ffddfe08053711c89ccb09f7b4b779
Will H <will.wkhu@gmail.com>
2023-01-06 10:55:13 -0800

    Work on code to detail knight path

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,10 @@
+#!/usr/bin/env node
+
+let startPt = [0,0];
+let endPt = [3, 3];
+let path = [];
+let queue = [];
+
 function moves(arr) {
   let [x,y] = arr;
 
@@ -15,5 +22,89 @@ function moves(arr) {
   
 }
 
-let start = [1,1];
-console.log(moves(start))
+class Node {
+  constructor(arr) {
+    this.position = arr;
+    this.moves = moves(arr);
+  }
+}
+
+let startNode = new Node(startPt);
+path.push(startNode.position);
+startNode.moves.forEach(mv => {
+  if (mv[0] == endPt[0] && mv[1] == endPt[1]) {
+    path.push(mv);
+  } else {
+    queue.push(new Node(mv));
+  }
+});
+
+if (queue.length > 0) {
+  
+}
+
+
+console.log(queue);
+// console.log('test')
+// path.forEach(nde => console.log(nde.position))
+
+// let start = [0, 0]; 
+
+// console.log(moves(start))
+
+/** */
+
+
+
+// class Node {
+//   constructor(arr) {
+//     let [x,y] = arr;
+//     this.position = arr;
+//     this.mv1 = this.checkVal(x + 1, y + 2);
+//     this.mv2 = this.checkVal(x + 1, y - 2);
+//     this.mv3 = this.checkVal(y + 2, x + 1);
+//     this.mv4 = this.checkVal(y - 2, x + 1);
+//     this.mv5 = this.checkVal(x - 1, y + 2);
+//     this.mv6 = this.checkVal(x - 1, y - 2);
+//     this.mv7 = this.checkVal(y + 2, x - 1);
+//     this.mv8 = this.checkVal(y - 2, x - 1);
+//   }
+
+//   checkVal(x,y) {
+//     if (x > 7 || x < 0) return null;
+//     if (y > 7 || y < 0) return null;
+//     return [x,y]
+//   }
+// }
+
+// class Tree {
+//   constructor(arr) {
+//     this.root = new Node(arr);
+//   }
+
+//   findPath(end, start = this.root) {
+//     let queue = [];
+//     let ans = [];
+//     let node = start;
+
+//     while (node.position[0] != end[0] && node.position[1] != end[1]) {
+//       ans.push(node)
+
+//       for (let i = 1; i < 9; i++) {
+//         if (node[`mv${i}`]) {
+//           // console.log(node[`mv${i}`])
+//           queue.push(new Node(node[`mv${i}`]));
+//         }
+//       }
+      
+//       node = queue.shift();
+//     }
+    
+//     return ans;
+//   }
+
+// }
+
+// let kTree = new Tree(startPt);
+// console.log(kTree.findPath(endPt));
+// console.log(kTree.root);
